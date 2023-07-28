8ddb485811db142b6195463dedf33a151dc172b7
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-07-29 03:02:15 +0530

    done

--- src/resources/views/main/pages/projects/detail.blade.php
+++ src/resources/views/main/pages/projects/detail.blade.php
@@ -763,6 +763,21 @@
 
     @include('main.includes.common_contact_modal_script')
 
+    <script src="https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/intlTelInput.min.js"></script>
+    <script nonce="{{ csp_nonce() }}" defer>
+    const countryData2 = window.intlTelInput(document.querySelector("#phone"), {
+        utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
+        autoInsertDialCode: true,
+        initialCountry: "auto",
+        geoIpLookup: callback => {
+            fetch("https://ipapi.co/json")
+            .then(res => res.json())
+            .then(data => callback(data.country_code))
+            .catch(() => callback("us"));
+        },
+    });
+    </script>
+
     <script type="text/javascript" nonce="{{ csp_nonce() }}" defer>
 
         let uuid = null;
@@ -842,6 +857,7 @@
                 formData.append('phone',document.getElementById('phone').value)
                 formData.append('subject',document.getElementById('subject').value)
                 formData.append('message',document.getElementById('message').value)
+                formData.append('country_code',countryData2.getSelectedCountryData().dialCode)
                 formData.append('page_url','{{Request::url()}}')
 
                 const response = await axios.post('{{route('contact_page.post')}}', formData)
