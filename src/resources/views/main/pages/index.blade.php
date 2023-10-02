fa538edeaa0a97d61c20f0ce7aeeebbad14f1dfd
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-10-02 16:44:18 +0530

    changed the ordering of scripts

--- src/resources/views/main/pages/index.blade.php
+++ src/resources/views/main/pages/index.blade.php
@@ -169,10 +169,10 @@
                             <a aria-label="{{$banners->banner_image_title}}" href="{{$banners->button_link ? $banners->button_link : '#'}}">
                                 {{-- <img src="{{$banners->banner_image_link}}" alt="{{$banners->banner_image_alt}}" title="{{$banners->banner_image_title}}" fetchpriority="high"> --}}
                                 <picture>
-                                    <source srcset="{{$banners->banner_mobile_image_link}}" media="(max-width: 600px)" class="">
-                                    <source srcset="{{$banners->banner_image_link}}" media="(max-width: 1920px)" class="">
-                                    <source srcset="{{$banners->banner_image_link}}" class="">
-                                    <img src="{{$banners->banner_image_link}}" alt="{{$banners->banner_image_alt}}" title="{{$banners->banner_image_title}}" fetchpriority="high" class="">
+                                    <source data-srcset="{{$banners->banner_mobile_image_link}}" media="(max-width: 600px)" class="lazyload">
+                                    <source data-srcset="{{$banners->banner_image_link}}" media="(max-width: 1920px)" class="lazyload">
+                                    <source data-srcset="{{$banners->banner_image_link}}" class="lazyload">
+                                    <img data-src="{{$banners->banner_image_link}}" alt="{{$banners->banner_image_alt}}" title="{{$banners->banner_image_title}}" fetchpriority="high" class="lazyload">
                                 </picture>
                             </a>
                         </div>
@@ -209,7 +209,7 @@
                 <div class="col-md-4 " data-animate-effect="fadeInUp">
                     @if($about->image)
                     <div class="con">
-                        <img src="{{$about->image_link}}" fetchpriority="high" class="img-fluid shapeee" alt="">
+                        <img data-src="{{$about->image_link}}" fetchpriority="high" class="img-fluid shapeee lazyload" alt="">
                     </div>
                     @endif
                 </div>
