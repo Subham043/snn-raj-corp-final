927f0665ec97941ab85a070e47d28eb6d4b0bb7f
Subham Saha <subham@Subhams-MacBook-Pro.local>
2024-05-23 18:28:28 +0530

    removed unwanted services

--- src/app/Providers/TelescopeServiceProvider.php
+++ src/app/Providers/TelescopeServiceProvider.php
@@ -19,7 +19,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         $this->hideSensitiveRequestDetails();
 
         Telescope::filter(function (IncomingEntry $entry) {
-            if ($this->app->environment('local') || $this->app->environment('production')) {
+            if ($this->app->environment('local')) {
                 return true;
             }
 
