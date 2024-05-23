0948fa00dda26cffeb9c84b78c1c55ebca02da75
Subham Saha <subham@Subhams-MacBook-Pro.local>
2024-05-23 22:19:47 +0530

    fixed

--- src/database/migrations/2023_05_20_051615_referal_enquiries.php
+++ src/database/migrations/2023_05_20_051615_referal_enquiries.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('referal_name', 500);
             $table->string('referal_email', 500);
             $table->string('referal_phone', 500)->nullable();
-            $table->string('country_code_1', 500)->nullable();
+            $table->string('country_code_2', 500)->nullable();
             $table->string('referal_relation', 500)->nullable();
             $table->foreignId('referal_project_id')->nullable()->constrained('projects')->nullOnDelete();
             $table->timestamps();
